be85b29bf9db3ce7e74a6bbee16988e1f632cbc3
Jangwon <wkddnjset@gmail.com>
2018-04-06 12:37:58 +0900

    Add Devices.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import AppBar from 'material-ui/AppBar';
 
-import Examples from './components/Examples/Examples.js';
+import Examples from './components/Examples/Examples';
+import BtnNav from './components/BottomNav/BtnNav';
+import SearchPage from './components/BottomNav/SearchPage';
+import ProfilePage from './components/BottomNav/ProfilePage';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-
-      <div className="App">
-      <MuiThemeProvider>
-        <AppBar
-          title="Example"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-        />
-        <p className="App-intro">
-        </p>
-        <Examples/>
-      </MuiThemeProvider>
-        
-      </div>
+      <Router>
+        <div className="App">
+          <MuiThemeProvider>
+            <AppBar
+              title="Example"
+              iconClassNameRight="muidocs-icon-navigation-expand-more"
+            />
+              <Switch>
+                <Route exact path="/" component={Examples} />
+                <Route exact path="/search" component={SearchPage} />
+                <Route exact path="/profile" component={ProfilePage} />
+              </Switch>
+            <BtnNav/>
+          </MuiThemeProvider>
+        </div>
+      </Router>
     );
   }
 }
